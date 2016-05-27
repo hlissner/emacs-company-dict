43c980c95992a6db6d48ecee8b0d84a8f746b5ac
Henrik Lissner <henrik@lissner.net>
2016-05-27 16:53:47 -0400

    v1.2.7 bump

--- company-dict.el
+++ company-dict.el
@@ -5,8 +5,8 @@
 ;; Author: Henrik Lissner <http://github/hlissner>
 ;; Maintainer: Henrik Lissner <henrik@lissner.net>
 ;; Created: June 21, 2015
-;; Modified: May 24, 2016
-;; Version: 1.2.6
+;; Modified: May 25, 2016
+;; Version: 1.2.7
 ;; Keywords: company dictionary ac-source-dictionary
 ;; Homepage: https://github.com/hlissner/emacs-company-dict
 ;; Package-Requires: ((company "0.8.12") (cl-lib "0.5") (parent-mode "2.3"))
